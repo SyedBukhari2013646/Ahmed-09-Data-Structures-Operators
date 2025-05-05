4a7e9d082c7b71218ae8fdcc11107d86bae04f13
SyedBukhari2013646 <syedbukhari03249334041@gmail.com>
2025-05-05 21:50:24 +0500

    Your descriptive commit message

--- starter/script.js
+++ starter/script.js
@@ -1,26 +1,26 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const italianFoods = new Set([
-  'pasta',
-  'gnocchi',
-  'tomatoes',
-  'olive oil',
-  'garlic',
-  'basil',
-]);
+// const italianFoods = new Set([
+//   'pasta',
+//   'gnocchi',
+//   'tomatoes',
+//   'olive oil',
+//   'garlic',
+//   'basil',
+// ]);
 
-const mexicanFoods = new Set([
-  'tortillas',
-  'beans',
-  'rice',
-  'tomatoes',
-  'avocado',
-  'garlic',
-]);
+// const mexicanFoods = new Set([
+//   'tortillas',
+//   'beans',
+//   'rice',
+//   'tomatoes',
+//   'avocado',
+//   'garlic',
+// ]);
 
 // Data needed for first part of the section
 const restaurant = {
@@ -34,55 +34,457 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  // openingHours: {
-  //   thu: {
-  //     open: 12,
-  //     close: 22,
-  //   },
-  //   fri: {
-  //     open: 11,
-  //     close: 23,
-  //   },
-  //   sat: {
-  //     open: 0, // Open 24 hours
-  //     close: 24,
-  //   },
-  // },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  orderDelivery: function ({
+    time = '20:00',
+    adress,
+    mainIndex = 0,
+    starterIndex,
+  }) {
+    console.log(
+      `Order Recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delievered to ${adress} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    `Here is your delecious pasta with ${ing1}, ${ing2} and ${ing3}`;
+  },
 };
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   adress: 'Mohalla Kucha Saadat Dhoke Fateh Attock Cantt',
+//   mainIndex: 2,
+//   starterIndex: 3,
+// });
 
-const [x, y, z] = arr;
-console.log(x);
-console.log(a);
-console.log(c);
-console.log(b);
-console.log(x, y, z);
+// restaurant.orderDelivery({
+//   adress: 'Mohalla Kucha Saadat Dhoke Fateh Attock Cantt',
+//   starterIndex: 2,
+// });
 
-console.log(arr);
+// // Destructuring Objects
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
 
-// according to restaurants
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
 
-let [first, hi, second] = restaurant.categories;
-console.log(first, hi, second);
+// // Default Values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
 
-[first, hi, second] = [second, first, hi];
-console.log(second, first, hi);
+// //Mutating Variables
+// let a = 123;
+// let b = 456;
+// const obj = { a: 23, b: 6, c: 9 };
+// ({ a, b } = obj);
+// console.log(a, b);
 
-// calling function order arrys
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// // Nested Object
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-// nested array
-const nested = [2, 3, [4, 5]];
+// const [x, y, z] = arr;
+// console.log(x);
+// console.log(a);
+// console.log(c);
+// console.log(b);
+// console.log(x, y, z);
 
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// console.log(arr);
 
-// Default values
+// // according to restaurants
 
-const [p, q, r] = [8, 9];
-console.log(p, q, r);
+// let [first, hi, second] = restaurant.categories;
+// console.log(first, hi, second);
+
+// [first, hi, second] = [second, first, hi];
+// console.log(second, first, hi);
+
+// // calling function order arrys
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// // nested array
+// const nested = [2, 3, [4, 5]];
+
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// // Default values
+
+// const [p, q, r] = [8, 9];
+// console.log(p, q, r);
+
+// The spread operator (...)
+
+const arr = [7, 8, 9];
+
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 3, 5, 2, 3);
+
+const newMenu = [...restaurant.mainMenu, 'Ginnoci'];
+console.log(newMenu);
+
+// Copy array
+const copyNewMenu = [...restaurant.mainMenu];
+console.log(copyNewMenu);
+
+// Join 2 array
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables are strings, maps, arrays, sets but NOT objects.
+const str = 'Syed Muhammad Ahmed Bukhari';
+const letters = [...str, ' ', 'Harris'];
+console.log(letters);
+console.log(str);
+
+// Real world Example
+const ingriedients = [
+  // prompt("Let's make pasta! Ingriedients 1?"),
+  // prompt('Ingriedients 2?'),
+  // prompt('Ingriedients 3'),
+];
+console.log(ingriedients);
+restaurant.orderPasta(...ingriedients);
+
+//Objects
+const newRestaurant = {
+  FoundedIn: 1991,
+  ...restaurant,
+  FoundedBy: 'Imran Khan',
+};
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Syed Bukhari';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+// Practicing Assignmets Questions
+
+const books = [
+  {
+    title: 'Algorithms',
+    author: ['Robert Sedgewick', 'Kevin Wayne'],
+    publisher: 'Addison-Wesley Professional',
+    publicationDate: '2011-03-24',
+    edition: 4,
+    keywords: [
+      'computer science',
+      'programming',
+      'algorithms',
+      'data structures',
+      'java',
+      'math',
+      'software',
+      'engineering',
+    ],
+    pages: 976,
+    format: 'hardcover',
+    ISBN: '9780321573513',
+    language: 'English',
+    programmingLanguage: 'Java',
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Structure and Interpretation of Computer Programs',
+    author: [
+      'Harold Abelson',
+      'Gerald Jay Sussman',
+      'Julie Sussman (Contributor)',
+    ],
+    publisher: 'The MIT Press',
+    publicationDate: '2022-04-12',
+    edition: 2,
+    keywords: [
+      'computer science',
+      'programming',
+      'javascript',
+      'software',
+      'engineering',
+    ],
+    pages: 640,
+    format: 'paperback',
+    ISBN: '9780262543231',
+    language: 'English',
+    programmingLanguage: 'JavaScript',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.36,
+        ratingsCount: 14,
+        reviewsCount: 3,
+        fiveStarRatingCount: 8,
+        oneStarRatingCount: 0,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+    publisher: 'Prentice Hall',
+    publicationDate: '2002-01-01',
+    edition: 1,
+    keywords: [
+      'computer science',
+      'computer systems',
+      'programming',
+      'software',
+      'C',
+      'engineering',
+    ],
+    pages: 978,
+    format: 'hardcover',
+    ISBN: '9780130340740',
+    language: 'English',
+    programmingLanguage: 'C',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 1010,
+        reviewsCount: 57,
+        fiveStarRatingCount: 638,
+        oneStarRatingCount: 16,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Operating System Concepts',
+    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+    publisher: 'John Wiley & Sons',
+    publicationDate: '2004-12-14',
+    edition: 10,
+    keywords: [
+      'computer science',
+      'operating systems',
+      'programming',
+      'software',
+      'C',
+      'Java',
+      'engineering',
+    ],
+    pages: 921,
+    format: 'hardcover',
+    ISBN: '9780471694663',
+    language: 'English',
+    programmingLanguage: 'C, Java',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 3.9,
+        ratingsCount: 2131,
+        reviewsCount: 114,
+        fiveStarRatingCount: 728,
+        oneStarRatingCount: 65,
+      },
+    },
+  },
+  {
+    title: 'Engineering Mathematics',
+    author: ['K.A. Stroud', 'Dexter J. Booth'],
+    publisher: 'Palgrave',
+    publicationDate: '2007-01-01',
+    edition: 14,
+    keywords: ['mathematics', 'engineering'],
+    pages: 1288,
+    format: 'paperback',
+    ISBN: '9781403942463',
+    language: 'English',
+    programmingLanguage: null,
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.35,
+        ratingsCount: 370,
+        reviewsCount: 18,
+        fiveStarRatingCount: 211,
+        oneStarRatingCount: 6,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'The Personal MBA: Master the Art of Business',
+    author: 'Josh Kaufman',
+    publisher: 'Portfolio',
+    publicationDate: '2010-12-30',
+    keywords: ['business'],
+    pages: 416,
+    format: 'hardcover',
+    ISBN: '9781591843528',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.11,
+        ratingsCount: 40119,
+        reviewsCount: 1351,
+        fiveStarRatingCount: 18033,
+        oneStarRatingCount: 1090,
+      },
+    },
+  },
+  {
+    title: 'Crafting Interpreters',
+    author: 'Robert Nystrom',
+    publisher: 'Genever Benning',
+    publicationDate: '2021-07-28',
+    keywords: [
+      'computer science',
+      'compilers',
+      'engineering',
+      'interpreters',
+      'software',
+      'engineering',
+    ],
+    pages: 865,
+    format: 'paperback',
+    ISBN: '9780990582939',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.7,
+        ratingsCount: 253,
+        reviewsCount: 23,
+        fiveStarRatingCount: 193,
+        oneStarRatingCount: 0,
+      },
+    },
+  },
+  {
+    title: 'Deep Work: Rules for Focused Success in a Distracted World',
+    author: 'Cal Newport',
+    publisher: 'Grand Central Publishing',
+    publicationDate: '2016-01-05',
+    edition: 1,
+    keywords: ['work', 'focus', 'personal development', 'business'],
+    pages: 296,
+    format: 'hardcover',
+    ISBN: '9781455586691',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.19,
+        ratingsCount: 144584,
+        reviewsCount: 11598,
+        fiveStarRatingCount: 63405,
+        oneStarRatingCount: 1808,
+      },
+    },
+    highlighted: true,
+  },
+];
+
+// // 1: Destructing arrays (1.1)
+
+// const [firstBook, secondBook] = books;
+
+// console.log(firstBook, secondBook);
+
+// // (1.2)
+
+// const [, , , thirdBook] = books;
+// console.log(thirdBook);
+
+// // (1.3)
+
+// const ratings = [
+//   ['rating', 4.19],
+//   ['ratingsCount', 144584],
+// ];
+
+// const [[, rating], [, ratingsCount]] = ratings;
+
+// console.log(rating, ratingsCount);
+
+// // (1.4)
+
+// const ratingStars = [63405, 1808];
+
+// const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+// console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+
+// 2.1: Destructing objects (1.1)
+// const { title, author, ISBN } = books[0];
+// console.log(title, author, ISBN);
+
+// // 2.2:
+// const { keywords: tags } = books[0];
+// console.log(tags);
+
+// // 2.3:
+// const { language, programmingLanguage = 'Unknown' } = books[6];
+// console.log(language, programmingLanguage);
+
+// // 2.4:
+// let bookTitle = 'unknown';
+// let bookAuthor = 'unknown';
+// ({ title: bookTitle, author: bookAuthor } = books[0]);
+// console.log(bookAuthor, bookTitle);
+// location;
+
+// // 2.5
+// const {
+//   thirdParty: {
+//     goodreads: { rating: bookRating },
+//   },
+// } = books[0];
+
+// // 2.6
+// function printBookInfo({ title, author, year }) {
+//   console.log(`${title} by ${author}, ${year}`);
+// }
+
+// printBookInfo({
+//   title: 'Algorithms',
+//   author: 'Robert Sedgewick',
+//   year: '2011',
+// });
+
+// The spread operator (...)
+
+// 3.1
